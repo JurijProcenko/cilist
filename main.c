a9a3ce3faaa63938a696ccabfa64952d996b04c0
Yurii Protsenko <elsoul@gmail.com>
2022-09-25 14:24:47 +0300

    main: Implement linked list traversal
    Iterate over all linked list nodes, initialize those and then print that
    out.
    
    Signed-off-by: Yurii Protsenko <elsoul@gmail.com>

--- main.c
+++ main.c
@@ -8,5 +8,19 @@ struct list {
 
 int main(void)
 {
+	struct list list[10], *head, *it;
+	size_t i;
+
+	head = &list[0];
+	for (i = 0; i < 9; ++i) {
+		list[i].next = &list[i+1];
+		list[i].val = i;
+	}
+	list[9].val = 9;
+	list[9].next = NULL;
+
+	for (it = head; it; it = it->next)
+		printf("%d\n", it->val);
+
 	return EXIT_SUCCESS;
 }
